e87cb8c8e0535117c2c103fe28e0c4eada99c334
Tomasz Wesołowski <kosashi@gmail.com>
2019-05-25 14:53:29 +0200

    Better example 02

--- 02-replacing.py
+++ 02-replacing.py
@@ -1,39 +1,58 @@
 import unittest
 import pytest
+from typing import List
 
 
-class MySystem:
-    def __init__(self):
-        self.value = 42
+class Hero:
+    def __init__(self, name: str):
+        self.name = name
+        self.level = 1
+        self.exp = 0
+        self.items: List[str] = []
 
-    def increment(self):
-        self.value += 1
-
-    def __str__(self):
-        return f"MySystem: {self.value}"
+    def add_exp(self, exp: int):
+        self.exp += exp
+        self.level = 1 + self.exp // 1000
 
 
-class MySystemTest(unittest.TestCase):
+class HeroTestCase(unittest.TestCase):
     def setUp(self):
-        self.system = MySystem()
+        self.hero = Hero("Merlin")
 
-    def test_str(self):
-        self.assertEqual(str(self.system), "MySystem: 42")
+    def test_hero_initial_level(self):
+        """Hero should have initial level = 1"""
+        self.assertEqual(1, self.hero.level)
 
-    def test_increment(self):
-        self.system.increment()
-        self.assertEqual(str(self.system), "MySystem: 43")
+    def test_hero_award_exp(self):
+        """Hero should accumulate exp"""
+        self.hero.add_exp(2500)
+        self.assertEqual(3, self.hero.level)
 
-
-def test_str(my_system):
-    assert str(my_system) == "MySystem: 42"
-
-
-def test_increment(my_system):
-    my_system.increment()
-    assert str(my_system) == "MySystem: 43"
+    def test_hero_level_up(self):
+        """Hero level should increase when levelling up"""
+        self.hero.add_exp(2500)
+        self.hero.add_exp(200)
+        self.assertEqual(2700, self.hero.exp)
 
 
 @pytest.fixture
-def my_system():
-    return MySystem()
+def hero():
+    return Hero("Merlin")
+
+
+def test_hero_initial_level(hero):
+    """Hero should have initial level = 1"""
+    assert 1 == hero.level
+
+
+def test_hero_award_exp(hero):
+    """Hero should accumulate exp"""
+    hero.add_exp(2500)
+    assert 3 == hero.level
+
+
+def test_hero_level_up(hero):
+    """Hero level should increase when levelling up"""
+    hero.add_exp(2500)
+    hero.add_exp(200)
+    assert 2700 == hero.exp
